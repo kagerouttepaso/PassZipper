0843b3f8456497a9aab34acef75adad7c8950650
kagerouttepaso <goth.wasawasa@gmail.com>
2015-10-02 20:41:07 +0900

    FirstCommit

---
+++ PassZipper/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassZipper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
